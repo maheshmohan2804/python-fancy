ed3a323c095b3b1c63f32adb172bef6ed667ac16
Mahesh M <maheshmohan2804@gmail.com>
2019-01-11 15:23:17 +0530

    Edit

--- fancy.py
+++ fancy.py
@@ -1,1 +1,14 @@
-jwdjn
+T = int(input())
+
+while T>0:
+    sen = input().split()
+    flag = 0
+    for word in sen:
+        if word=="not":
+            flag = 1
+            break
+    if flag==1:
+        print("Real Fancy")
+    else:
+        print("regularly fancy")
+    T=T-1
